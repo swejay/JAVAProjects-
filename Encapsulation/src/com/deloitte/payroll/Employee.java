f0a1e65b551378564734befa14116939c92033d8
swetha1 <swetha1@192.168.1.1>
2018-07-25 17:25:36 -0700

    commit encap

---
+++ Encapsulation/src/com/deloitte/payroll/Employee.java
@@ -0,0 +1,33 @@
+package com.deloitte.payroll;
+
+import java.sql.Date;
+
+public class Employee {
+	
+	public Date date;
+	
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private int deptId;
+	public int getEmpId() {
+		return empId;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public int getDeptId() {
+		return deptId;
+	}
+	
+	
+
+
+}
