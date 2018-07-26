af472e3111524353e17ae0a9fc1fb7ae3a5fb29b
swetha1 <41478071+swetha1@users.noreply.github.com>
2018-07-26 17:40:14 +0530

    commit exceptions

---
+++ ExceptionHandling/src/com/deloitte/exceptions/main/MainClass.java
@@ -0,0 +1,20 @@
+package com.deloitte.exceptions.main;
+
+public class MainClass {
+ public static void main(String[] param) {
+	 try {
+
+		System.out.println(param[0]+param[1]); // this will concatenate the 2 
+
+	 System.out.println(Integer.parseInt(param[0])+ Integer.parseInt(param[1]));
+	 } //    this adds the two values since we are converting string type to integer
+	 
+	 catch(ArrayIndexOutOfBoundsException e ) {
+		 System.out.println("Please enter 2 values");
+
+	 }
+	 finally {
+	 System.out.println("end");
+}
+ }
+ }
